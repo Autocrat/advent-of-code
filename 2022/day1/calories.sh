b3e097bcd51eff2002b0b17e823ebf57d93f5b9f
Tom Chalker <tomchalker@gmail.com>
2022-12-02 18:58:02 -0500

    Combine all into 1, get final answers

--- 2022/day1/calories.sh
+++ 2022/day1/calories.sh
@@ -11,21 +11,23 @@ while read -r LINE
 do
     if [ -n "${LINE}" ]
     then
-    ((CALORIES[${ELF_INDEX}]+=$LINE))
+	((CALORIES[${ELF_INDEX}]+=$LINE))
     if [[ {${CALORIES[${ELF_INDEX}]}} > ${MAX_CALORIES} ]]
     then
         MAX_CALORIES={${CALORIES[${ELF_INDEX}]}}
         MAX_CALORIES_ELF=${ELF_INDEX}
     fi
     else
-    CALORIES[$((++ELF_INDEX))]=0    
+	CALORIES[$((++ELF_INDEX))]=0    
     fi
 done < ${INPUTFILE}
 
 
-for ((i=0;i<${#CALORIES[*]};i++))
-do
-    echo Elf number $i is carrying ${CALORIES[i]}
-done
+echo ${CALORIES[@]} | tr ' ' '\n' | sort -n | tail -3 | tee top3.txt
 
-echo Elf ${MAX_CALORIES_ELF} is carrying the most with ${CALORIES[${MAX_CALORIES_ELF}]}
+TOP_THREE_TOTAL=0
+while read LINE
+do
+    ((TOP_THREE_TOTAL+=${LINE}))
+done < top3.txt
+echo ${TOP_THREE_TOTAL}
