f4cdc5cc860ed0dc9a11fb53a8518efc10e792b8
Tom Chalker <tomchalker@gmail.com>
2022-12-01 21:19:18 -0500

    Added sorted top 3

---
+++ 2022/day1/calories2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+INPUTFILE=CalorieFile.txt
+
+ELF_INDEX=0
+CALORIES[${ELF_INDEX}]=0
+MAX_CALORIES=0
+MAX_CALORIES_ELF=0
+
+while read -r LINE
+do
+    if [ -n "${LINE}" ]
+    then
+	((CALORIES[${ELF_INDEX}]+=$LINE))
+    if [[ {${CALORIES[${ELF_INDEX}]}} > ${MAX_CALORIES} ]]
+    then
+        MAX_CALORIES={${CALORIES[${ELF_INDEX}]}}
+        MAX_CALORIES_ELF=${ELF_INDEX}
+    fi
+    else
+	CALORIES[$((++ELF_INDEX))]=0    
+    fi
+done < ${INPUTFILE}
+
+
+echo ${CALORIES[@]} | tr ' ' '\n' | sort -n | tail -3
