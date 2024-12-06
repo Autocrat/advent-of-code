76ab800448d4957a2c5dd5f36f0a7c2c8346b0fb
Tom Chalker <tomchalker@gmail.com>
2024-12-06 18:38:04 -0500

    Close but no cigar

--- 2024/day2/day2b/src/main.rs
+++ 2024/day2/day2b/src/main.rs
@@ -18,35 +18,51 @@ fn main() {
         let mut prevDelta = 0;
         let mut safe = true;
         let mut offset = 0;
-        
+        let mut bustedOnce = false;
+
         for i in 1..report.len()
         {
 //            println!{"Element: {}  {}", i, i-1-offset};
             let currentDelta = report[i] - report[i-1-offset];
             
             if currentDelta.abs() > 3 {
+                println!("Fail at too far");
                 safe = false; 
             } else if currentDelta == 0 {
+                println!("Fail at too close");
                 safe = false; 
             } else if prevDelta < 0 && currentDelta > 0 {
+                println!{"Fail at deltas 1: {}   {}", prevDelta, currentDelta};
+                  if i == 2
+                  { prevDelta = 0;}
                 safe = false; 
             } else if prevDelta > 0 && currentDelta < 0 {
+                println!{"Fail at deltas 2: {}   {}", prevDelta, currentDelta};
+                  if i == 2
+                  { prevDelta = 0;}
                 safe = false; 
             } else {
                 prevDelta = currentDelta;
+                offset = 0;
             }
-            
-            if ! safe && offset == 0 && i > 1 {
-                safe = true;
-                offset = 1;
-            }            
 
+            if !safe && bustedOnce == false {
+                safe = true;
+                bustedOnce = true;
+
+                if i > 2 {
+                    offset = 1;
+                    println!("Setting offset");
+                }
+            }
         }
 
         if safe {
             safeCount += 1;
-        } 
-    
+            println!{"{:?}", report};
+} 
+        else {
+        }    
     }
     println!("Safecount: {}", safeCount);
 
